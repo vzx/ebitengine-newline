69a8b04a8f8683d8d8b2f092ea59cea6547e1b70
vzx <vzx>
2024-12-13 22:14:36 +0100

    add v1

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/vzx/ebitengine-newline
 
 go 1.23.4
 
-require github.com/hajimehoshi/ebiten/v2 v2.9.0-alpha.3.0.20241211150535-52be3859c0a2
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.9.0-alpha.3.0.20241211150535-52be3859c0a2
+	golang.org/x/image v0.23.0
+)
 
 require (
 	github.com/ebitengine/gomobile v0.0.0-20241016134836-cc2e38a7c0ee // indirect
@@ -10,7 +13,6 @@ require (
 	github.com/ebitengine/purego v0.9.0-alpha.1 // indirect
 	github.com/go-text/typesetting v0.2.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
-	golang.org/x/image v0.23.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
-	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	textv1 "github.com/hajimehoshi/ebiten/v2/text"
+	textv2 "github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 )
 
-var mplusFaceSource *text.GoTextFaceSource
+var mplusFace font.Face
+var mplusFaceSource *textv2.GoTextFaceSource
 
 type Game struct{}
 
@@ -19,14 +23,14 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	msg := "The quick\n\tbrown fox jumps\n\n\t\tover the lazy\r\n\rdog."
-	op := &text.DrawOptions{}
-	op.GeoM.Translate(10, 40)
+	textv1.Draw(screen, "Hello, \nv1 world", mplusFace, 10, 40, color.White)
+
+	msg := "Hello,\nv2 world"
+	op := &textv2.DrawOptions{}
+	op.GeoM.Translate(500, 40)
 	op.ColorScale.ScaleWithColor(color.White)
-	text.Draw(screen, msg, &text.GoTextFace{
-		Source: mplusFaceSource,
-		Size:   32,
-	}, op)
+	textv2.Draw(screen, msg, &textv2.GoTextFace{Source: mplusFaceSource, Size: 32}, op)
+
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -35,12 +39,28 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func main() {
 	var err error
-	if mplusFaceSource, err = text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf)); err != nil {
-		log.Fatal(err)
-	}
+
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	fatalIfErr(err)
+
+	mplusFace, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    32,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	fatalIfErr(err)
+
+	mplusFaceSource, err = textv2.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	fatalIfErr(err)
 
 	ebiten.SetWindowSize(1280, 720)
 	if err = ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fatalIfErr(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
 }
